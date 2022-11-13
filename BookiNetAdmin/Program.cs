95c1ce94f47c9e2d81e5dcd8d3f7cd030d23eebd
Fabio Luiz Alvares Osti <fabio.osti@hotmail.com>
2022-11-13 18:02:23 -0300

    Refactor

--- BookiNetAdmin/Program.cs
+++ BookiNetAdmin/Program.cs
@@ -3,20 +3,51 @@ using FirebaseAdmin;
 using FirebaseAdmin.Auth;
 using Google.Apis.Auth.OAuth2;
 
-FirebaseApp.Create(new AppOptions()
+internal class Program
 {
-	Credential = GoogleCredential.FromFile("./boookie-28f8d-firebase-adminsdk-8k7t3-913cf19704.json"),
-});
-
-UserRecord user = await FirebaseAuth.DefaultInstance
-	.GetUserByEmailAsync(args[0]);
-// Confirm user is verified.
-if (user.EmailVerified) {
-	var isNotAdmin = !user.CustomClaims.Keys.Contains("admin");
-	var claims = isNotAdmin ? new Dictionary<string, object>()
+	private static async Task Create(string email, string password)
 	{
-		{ "admin", true },
-	} : new Dictionary<string, object>();
-	await FirebaseAuth.DefaultInstance.SetCustomUserClaimsAsync(user.Uid, claims);
-	Console.WriteLine($"User {(isNotAdmin ? "promoted" : "demoted")}.");
+		UserRecordArgs uArgs = new()
+		{
+			Email = email,
+			Password = password
+		};
+		UserRecord user = await FirebaseAuth.DefaultInstance.CreateUserAsync(uArgs);
+		await FirebaseAuth.DefaultInstance.SetCustomUserClaimsAsync(
+			user.Uid, new Dictionary<string, object>() { { "admin", true } });
+	}
+
+	private static async Task Modify(string email, bool demote)
+	{
+		UserRecord user = await FirebaseAuth.DefaultInstance.GetUserByEmailAsync(email);
+		await FirebaseAuth.DefaultInstance.SetCustomUserClaimsAsync(
+			user.Uid, demote ? 
+				new Dictionary<string, object>() 
+				: new Dictionary<string, object>() { { "admin", true } });
+
+	}
+
+	private static async Task<int> Main(string[] args)
+	{
+		FirebaseApp.Create(new AppOptions()
+		{
+			Credential = GoogleCredential.FromFile("./boookie-28f8d-firebase-adminsdk-8k7t3-913cf19704.json"),
+		});
+
+		switch (args[0]) {
+			case "--c":
+				await Create(args[1], args[2]);
+				break;
+			case "--p":
+				await Modify(args[1], false);
+				break;
+			case "--d":
+				await Modify(args[1], true);
+				break;
+			default:
+				throw new ArgumentException("Invalid option");
+		};
+
+		return 0;
+	}
 }
